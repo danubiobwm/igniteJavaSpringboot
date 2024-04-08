5323f3029dc85fcf75cc9833d91cb7dd0cb5bad4
danubiobwm <danubio.bwm@gmail.com>
2024-04-08 11:34:30 -0300

    Java Wrapper

---
+++ Fundamentos_Java/wrapper/Wrapper.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class Wrapper {
+    int tipoInt = 1;
+    float tipoFloat= 1;
+    boolean tipoBoolean = false;
+
+    // int - char
+    Float tipoFloat2 = 3F;
+    Boolean tipoBoolean2 = False;
+    Integer tipoInterger2 = 2;
+
+}
