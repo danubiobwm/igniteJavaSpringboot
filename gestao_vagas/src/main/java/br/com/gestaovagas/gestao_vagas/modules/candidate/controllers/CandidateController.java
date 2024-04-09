23f1cb7aea9b96bd9911121be9516be38f291263
Danubio de Araujo <danubio.bwm@gmail.com>
2024-04-09 01:33:04 -0300

    create repository

--- gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,9 @@
 package br.com.gestaovagas.gestao_vagas.modules.candidate.controllers;
 
 import br.com.gestaovagas.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.gestaovagas.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+  @Autowired
+  private CandidateRepository candidateRepository;
+
   @PostMapping("/")
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity){
-    System.out.println("Candidato");
-    System.out.println(candidateEntity.getName());
-    System.out.println(candidateEntity.getUsername());
-    System.out.println(candidateEntity.getEmail());
-    System.out.println(candidateEntity.getPassword());
-    System.out.println(candidateEntity.getDescription());
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+    return this.candidateRepository.save(candidateEntity);
   }
 
 }
