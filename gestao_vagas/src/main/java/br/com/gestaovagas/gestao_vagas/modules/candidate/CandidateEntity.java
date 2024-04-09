63f97a0253fafb796aa19d40ba726db4308efd75
Danubio de Araujo <danubio.bwm@gmail.com>
2024-04-09 01:23:55 -0300

    create table candidato

--- gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,12 +1,19 @@
 package br.com.gestaovagas.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
+import lombok.Data;
 
 
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
@@ -14,8 +21,12 @@ import java.util.UUID;
  */
 
 @Data
+@Entity(name="candidate")
 public class CandidateEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
   @NotBlank()
@@ -29,4 +40,7 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createAt;
 }
