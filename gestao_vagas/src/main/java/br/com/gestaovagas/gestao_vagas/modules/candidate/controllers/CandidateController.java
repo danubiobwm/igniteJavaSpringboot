5476be8b7ca8148cc10ed229ed395144778a925f
danubiobwm <danubio.bwm@gmail.com>
2024-04-12 10:26:59 -0300

    Swagger finishe

--- gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.gestaovagas.gestao_vagas.modules.candidate.controllers;
 
+import br.com.gestaovagas.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.gestaovagas.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import br.com.gestaovagas.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import br.com.gestaovagas.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
@@ -29,6 +30,8 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
+
 public class CandidateController {
 
   private CandidateRepository candidateRepository;
@@ -43,6 +46,13 @@ public class CandidateController {
   private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
   @PostMapping("/")
+  @Operation(summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar um candidato")
+  @ApiResponses({
+          @ApiResponse(responseCode = "200", content = {
+                  @Content(schema = @Schema(implementation = CandidateEntity.class))
+          }),
+          @ApiResponse(responseCode = "400", description = "Usuário já existe")
+  })
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
     try {
       var result = this.createCandidateUseCase.execute(candidateEntity);
@@ -54,6 +64,14 @@ public class CandidateController {
 
   @GetMapping("/")
   @PreAuthorize("hasRole('CANDIDATE')")
+  @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
+  @ApiResponses({
+          @ApiResponse(responseCode = "200", content = {
+                  @Content(schema = @Schema(implementation = ProfileCandidateResponseDTO.class))
+          }),
+          @ApiResponse(responseCode = "400", description = "User not found")
+  })
+  @SecurityRequirement(name = "jwt_auth")
   public ResponseEntity<Object> get(HttpServletRequest request) {
     var idCandidate = request.getAttribute("candidate_id");
 
@@ -68,7 +86,6 @@ public class CandidateController {
 
   @GetMapping("/job")
   @PreAuthorize("hasRole('CANDIDATE')")
-  @Tag(name = "Candidato", description = "Informações do candidato")
   @Operation(summary = "Listagem de vagas disponível para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro")
   @SecurityRequirement(name = "jwt_auth")
   @ApiResponses({
--- gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -1,5 +1,6 @@
 package br.com.gestaovagas.gestao_vagas.modules.candidate.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,17 +28,23 @@ public class CandidateEntity {
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
+  @Schema(example = "Daniel de Souza", requiredMode = Schema.RequiredMode.REQUIRED, description = "Nome do candidato")
   private String name;
 
   @NotBlank()
   @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+  @Schema(example = "daniel", requiredMode = Schema.RequiredMode.REQUIRED, description = "Username do candidato")
   private String username;
 
   @Email (message = "O campo [email] deve conter um e-mail válido")
+  @Schema(example = "daniel", requiredMode = Schema.RequiredMode.REQUIRED, description = "Username do candidato")
   private String email;
 
   @Length(min=10, max=100)
+  @Schema(example = "admin@1234", minLength = 10, maxLength = 100, requiredMode = Schema.RequiredMode.REQUIRED, description = "Senha do candidato")
   private String password;
+
+  @Schema(example = "Desenvolvedor Java", requiredMode = Schema.RequiredMode.REQUIRED, description = "Breve descrição do candidato")
   private String description;
   private String curriculum;
 
